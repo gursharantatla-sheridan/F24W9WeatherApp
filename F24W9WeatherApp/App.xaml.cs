59353d33be13ea816cefe23b235fcbca169c7b70
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-11-05 14:57:41 -0500

    Add project files.

---
+++ F24W9WeatherApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace F24W9WeatherApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ F24W9WeatherApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace F24W9WeatherApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
